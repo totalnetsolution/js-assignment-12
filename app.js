ba471a765cbe95a7a4223b2dc40d4a016ed9acba
muhammad-danish <totalnetsolution2@gmail.com>
2024-03-01 04:28:23 +0500

    index file created

---
+++ app.js
@@ -0,0 +1,234 @@
+// var num; prompt("Please Enter any number");
+// if(num % 3 == 0){
+//     console.log('number is divisible by 3');
+// }else{
+//     console.log('number is not divisible by 3');
+// }
+
+
+// var num = +prompt("Please Enter your number")
+// if(num > 5 ){
+//     console.log("Number is valid")
+// }
+// else{
+//     console.log('Number is invalid')
+// }
+
+
+
+
+
+
+// var height = +prompt("My Name is Muhammad Danish")
+// if(height < 20){
+//     console.log("Not Accepted")
+// }
+// else if( height > 30 ){
+//   console.log("Accepted")
+// }
+// else{
+//     console.log("Not Aplicable")
+// }
+
+
+
+
+
+
+
+// var salery = +prompt("My Income")
+// if(salery >= 100000){
+//     console.log("I am owner of Crown Home Appliances")
+// }
+// else if(salery<=35000){
+//    console.log("My Sales person")
+// }
+// else{
+//     console.log("Please Enter a valid salary")
+// }
+
+
+
+
+
+
+// var Age = +prompt("Enter your age" )
+// if( Age >= 24 ){
+//     console.log("Applicable")
+// }
+// else if( 26 <= Age  ){
+//    console.log("Accepted")
+// }
+// else if( 22 <= Age  ){
+//     console.log("Not Applicable")
+//  }
+// else{
+//     console.log("please enter a valid age")
+// }
+
+
+
+
+
+
+
+
+var num = +prompt("what is your income")
+if( num > 100000 ){
+    console.log("Good")
+}
+
+else{
+    console.log("Keep it up")
+}
+
+
+
+
+
+
+
+// <///------------------------ 2 nd scripts start-------------------------------///>
+
+
+
+
+// alert("hello world")
+// alert("Muhammad/Danish")
+
+// 4 tarha ki data types hoti ha
+
+// 1 string "ejdsn4383029"
+// 2 number 6738
+// 3Boolean true ya false
+// 4 ko bada dakhanga
+// var userName = ("Muhammad danish")
+// var age = (40)
+// var education = ("S.E")
+// var address = ("F.B.Area")
+// console.log(userName);
+// console.log(age);
+// console.log(education);
+// console.log(address);
+// console.log(userName + " " + age + " " + education + " " + address)
+// var Age1 = 62;
+// var Age2 = 63;
+// var Age3 = "64";
+// var Age4 = "65";
+// console.log(Age2 + Age1);
+// console.log(Age2 + " " + Age1);
+// console.log(Age3 + " " + Age4);
+// console.log(Age3 + + Age4);
+// var Age1 = prompt("enter first number");
+// var Age2 = prompt("enter second number");
+// var Age3 = +prompt("enter THIRD number");
+// var Age4 = +prompt("enter fourth number");
+
+
+// console.log(Age1 + Age2);
+// console.log(Age3 + Age4);
+// var num1 = 20;
+// num1=num1+10
+// line number 34 ka 1 short form bhi ha wo nicha likhi ha
+
+
+
+
+
+// num1 += 10
+// num1-=10
+// num1*=10
+// num1/=10
+// console.log(num1 + 15);
+// console.log(10 < 5);
+// console.log(10 >= 10);
+// console.log(10 <= 10);
+// console.log(10 >= "10");
+// console.log(9 <= "10");
+// // xclination mark (!) ka madlab ha not
+// console.log(10 != 10);
+// console.log(10 == "10");
+// console.log(10 !== "10");
+// console.log(10 !== 10);
+
+
+
+
+// double equal to data type bhi check krta ha (==) LockManager(!) ka sath
+// && = and
+// || = OR
+
+
+
+// var comparisions = 10 <= 10 || 10 < 6;
+// console.log(comparisions)
+
+
+
+
+// (|| = OR)jitni bhi conditions ho unma sa agar 1 bhi true hui to wo nicha true batada ga answer ma
+// (& = and) is parallel of OR
+
+
+
+
+// var comparision = 10 >= 10 && 10 > 6;
+// console.log(comparision);
+
+
+// hum prom ma or console ma bracket () ka ander koi chez inverted commas "" ma jabhi likhta ha jub hum na wo chez first time likhi ho or baghir "" ka tub likhanga jub wo chez uper humna console ma ya var am likhi hui ho uper
+
+// var english = +prompt("english marks");
+// var math = +prompt ("math marks");
+// var obtain = +(english + math);
+// var totalmarks = 200;
+// console.log(obtain);
+// console.log(totalmarks);
+// console.log(obtain/totalmarks*100+"%")
+
+
+
+
+// var english = +prompt("english marks");
+// var math = +prompt("math marks");
+// var obtain= english + math;
+// var totalmarks= 200;
+// console.log(obtain);
+// console.log(totalmarks);
+// console.log(obtain/totalmarks*100 + "%");
+
+
+
+
+
+
+
+// if condition is true than program run if otherwise else will run
+
+// var persentage = +prompt("what is your persentage")
+// if (persentage >= 50){
+//     console.log("Passed")
+// }
+// else{
+//     console.log("Failed")
+// }
+
+
+
+// var num = +prompt("Please Enter your number")
+// if(num > 5 ){
+//     console.log("ifi") 
+// }
+// else{
+//     console.log('elses')
+// }
+
+
+
+// var persentage = +prompt("what is your persentage")
+// if (persentage >= 50){
+//     console.log("Passed")
+// }
+// else{
+//     console.log("Failed")
+// }
